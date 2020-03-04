28801c3618f0f71f18b5e82029774098e67e7de3
Josef Fürlinger <josef.fuerlinger@smartpoint.at>
2020-03-04 17:24:05 +0100

    Add query for instrumentation key

--- all_outputs.tf
+++ all_outputs.tf
@@ -5,3 +5,7 @@ output "target_azure_subscription" {
   value = "${data.azurerm_subscription.current.display_name}"
 }
 
+output "instrumentation_key" {
+  value = "${azurerm_application_insights.ai.instrumentation_key}"
+}
+
